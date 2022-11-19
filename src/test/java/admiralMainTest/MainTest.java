f85b228923096d56a5055348ab7b01e0f57e0c0e
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-11-20 01:24:03 +0200

    first test, albeit unnecessary

---
+++ src/test/java/admiralMainTest/MainTest.java
@@ -0,0 +1,13 @@
+package admiralMainTest;
+
+import main.Main;
+
+/**
+ * A dummy test that launches the main method itself (argumentless).
+ */
+public class MainTest {
+    //TODO: add javadoc
+    public static void main(String[] args) {
+        Main.main(null);
+    }
+}
