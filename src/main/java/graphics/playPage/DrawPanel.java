3d34d8b42eb90a98bff888d1a0dd2231fa0abf66
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-11-21 23:08:38 +0200

    observer info

--- src/main/java/graphics/playPage/DrawPanel.java
+++ src/main/java/graphics/playPage/DrawPanel.java
@@ -12,93 +12,18 @@ import org.jetbrains.annotations.NotNull;
 //TODO: add javadocs
 public class DrawPanel extends DynamicPanel {
     private static final Color BACKGROUND_COLOR = new Color(50, 100, 150);
-    private static final int[] ZOOM_LIMITS = new int[] {1, 10};
-    private static final int INITIAL_ZOOM = 5, ZOOM_INCREMENT = 1;
-    private final int[] mousePos = new int[2];
-    private int zoom = INITIAL_ZOOM;
-    private boolean panelActive = false;
+
+    boolean panelActive = false;
+    private ObserverInfo observerInfo;
 
     //TODO: add javadoc
     DrawPanel() {
         super(new SimpleColorScheme(BACKGROUND_COLOR, null));
-        addMouseMotionListener(getNewMouseMotionListener());
-        addMouseWheelListener(getNewMouseWheelListener());
-        addMouseListener(getNewMouseListener());
+        observerInfo = new ObserverInfo(this);
+        addMouseMotionListener(observerInfo.getNewMouseMotionListener());
+        addMouseWheelListener(observerInfo.getNewMouseWheelListener());
+        addMouseListener(observerInfo.getNewMouseListener());
         //setBackground(backgroundColor);
-    }
-
-    private @NotNull MouseMotionListener getNewMouseMotionListener() {
-        return new MouseMotionListener() {
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                if (panelActive) {
-                    mousePos[0] = e.getX();
-                    mousePos[1] = e.getY();
-                }
-            }
-
-            @Override
-            public void mouseMoved(MouseEvent e) {
-                if (panelActive) {
-                    mousePos[0] = e.getX();
-                    mousePos[1] = e.getY();
-                }
-            }
-        };
-    }
-
-    private @NotNull MouseWheelListener getNewMouseWheelListener() {
-        return new MouseWheelListener() {
-            @Override
-            public void mouseWheelMoved(MouseWheelEvent e) {
-                if (panelActive) {
-                    zoom = getNewZoom(zoom, e.getWheelRotation() * ZOOM_INCREMENT);
-                }
-            }
-
-            private int getNewZoom(int previous, int delta) {
-                return Math.max(
-                        ZOOM_LIMITS[0],
-                        Math.min(
-                                ZOOM_LIMITS[1],
-                                previous + delta));
-            }
-        };
-    }
-
-    private @NotNull MouseListener getNewMouseListener() {
-        return new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                mouseClickAction(e);
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                //
-            }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                mouseClickAction(e);
-            }
-
-            private void mouseClickAction(MouseEvent e) {
-                if (panelActive) {
-                    StaticData.clickPoints.add(new int[] {e.getX(), e.getY()});
-                }
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                panelActive = true;
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                panelActive = false;
-            }
-        };
     }
 
     //TODO: add javadoc
@@ -108,19 +33,30 @@ public class DrawPanel extends DynamicPanel {
         Dimension panelSize = this.getSize();
         g.setColor(Color.red);
         g.drawLine(0, 0, panelSize.width, panelSize.height);
-        drawClickPoints(g);
-        drawMouseInfo(g);
+        if (observerInfo == null) {
+            drawUninitializedObserver(g);
+        } else {
+            drawClickPoints(g);
+            drawMouseInfo(g);
+        }
+    }
+
+    private void drawUninitializedObserver(@NotNull Graphics g) {
+        int[] textPosition = new int[] {10, 25};
+        g.setColor(getPanelColors().getSecondaryColor());
+        g.drawString("Observer not initialized.", textPosition[0], textPosition[1]);
     }
 
     private void drawMouseInfo(@NotNull Graphics g) {
         g.setColor(getPanelColors().getSecondaryColor());
+        int[] mousePos = observerInfo.mousePos;
         g.drawString("x: " + mousePos[0] + ", y: " + mousePos[1], mousePos[0], mousePos[1]);
-        g.drawString("zoom: " + zoom, mousePos[0], mousePos[1] + 15);
+        g.drawString("zoom: " + observerInfo.zoom, mousePos[0], mousePos[1] + 15);
     }
 
     private void drawClickPoints(@NotNull Graphics g) {
         int radius = 10;
-        g.setColor(Color.red);
+        g.setColor(new Color(100, 70, 0));
         for (int[] point : StaticData.clickPoints) {
             g.fillOval(
                     point[0] - radius / 2,
---
+++ src/main/java/graphics/playPage/ObserverInfo.java
@@ -0,0 +1,94 @@
+package graphics.playPage;
+
+import graphics.staticData.StaticData;
+
+import java.awt.event.*;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ObserverInfo {
+    private static final int[] ZOOM_LIMITS = new int[] {1, 10};
+    private static final int INITIAL_ZOOM = 5, ZOOM_INCREMENT = 1;
+
+    private final DrawPanel drawPanel;
+    final int[] mousePos = new int[2];
+    int zoom = INITIAL_ZOOM;
+
+    ObserverInfo(@NotNull DrawPanel drawPanel) {
+        this.drawPanel = drawPanel;
+    }
+
+    @NotNull MouseMotionListener getNewMouseMotionListener() {
+        return new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if (drawPanel.panelActive) {
+                    mousePos[0] = e.getX();
+                    mousePos[1] = e.getY();
+                }
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                if (drawPanel.panelActive) {
+                    mousePos[0] = e.getX();
+                    mousePos[1] = e.getY();
+                }
+            }
+        };
+    }
+
+    @NotNull MouseWheelListener getNewMouseWheelListener() {
+        return new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                if (drawPanel.panelActive) {
+                    zoom = getNewZoom(zoom, e.getWheelRotation() * ZOOM_INCREMENT);
+                }
+            }
+
+            private int getNewZoom(int previous, int delta) {
+                return Math.max(
+                        ZOOM_LIMITS[0],
+                        Math.min(
+                                ZOOM_LIMITS[1],
+                                previous + delta));
+            }
+        };
+    }
+
+    @NotNull MouseListener getNewMouseListener() {
+        return new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                mouseClickAction(e);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                //
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mouseClickAction(e);
+            }
+
+            private void mouseClickAction(MouseEvent e) {
+                if (drawPanel.panelActive) {
+                    StaticData.clickPoints.add(new int[] {e.getX(), e.getY()});
+                }
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                drawPanel.panelActive = true;
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                drawPanel.panelActive = false;
+            }
+        };
+    }
+}
