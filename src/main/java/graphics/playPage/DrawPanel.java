d2f2bf3734aba9dcf92a87ce131c64c89ecce8ad
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-11-21 22:48:50 +0200

    clickable points

--- src/main/java/graphics/playPage/DrawPanel.java
+++ src/main/java/graphics/playPage/DrawPanel.java
@@ -1,5 +1,6 @@
 package graphics.playPage;
 
+import graphics.staticData.StaticData;
 import graphicsEngine.colors.SimpleColorScheme;
 import graphicsEngine.panels.DynamicPanel;
 
@@ -15,12 +16,14 @@ public class DrawPanel extends DynamicPanel {
     private static final int INITIAL_ZOOM = 5, ZOOM_INCREMENT = 1;
     private final int[] mousePos = new int[2];
     private int zoom = INITIAL_ZOOM;
+    private boolean panelActive = false;
 
     //TODO: add javadoc
     DrawPanel() {
         super(new SimpleColorScheme(BACKGROUND_COLOR, null));
         addMouseMotionListener(getNewMouseMotionListener());
         addMouseWheelListener(getNewMouseWheelListener());
+        addMouseListener(getNewMouseListener());
         //setBackground(backgroundColor);
     }
 
@@ -28,14 +31,18 @@ public class DrawPanel extends DynamicPanel {
         return new MouseMotionListener() {
             @Override
             public void mouseDragged(MouseEvent e) {
-                mousePos[0] = e.getX();
-                mousePos[1] = e.getY();
+                if (panelActive) {
+                    mousePos[0] = e.getX();
+                    mousePos[1] = e.getY();
+                }
             }
 
             @Override
             public void mouseMoved(MouseEvent e) {
-                mousePos[0] = e.getX();
-                mousePos[1] = e.getY();
+                if (panelActive) {
+                    mousePos[0] = e.getX();
+                    mousePos[1] = e.getY();
+                }
             }
         };
     }
@@ -44,7 +51,9 @@ public class DrawPanel extends DynamicPanel {
         return new MouseWheelListener() {
             @Override
             public void mouseWheelMoved(MouseWheelEvent e) {
-                zoom = getNewZoom(zoom, e.getWheelRotation() * ZOOM_INCREMENT);
+                if (panelActive) {
+                    zoom = getNewZoom(zoom, e.getWheelRotation() * ZOOM_INCREMENT);
+                }
             }
 
             private int getNewZoom(int previous, int delta) {
@@ -57,6 +66,41 @@ public class DrawPanel extends DynamicPanel {
         };
     }
 
+    private @NotNull MouseListener getNewMouseListener() {
+        return new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                mouseClickAction(e);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                //
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mouseClickAction(e);
+            }
+
+            private void mouseClickAction(MouseEvent e) {
+                if (panelActive) {
+                    StaticData.clickPoints.add(new int[] {e.getX(), e.getY()});
+                }
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                panelActive = true;
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                panelActive = false;
+            }
+        };
+    }
+
     //TODO: add javadoc
     @Override
     protected void paintComponent(Graphics g) {
@@ -64,8 +108,25 @@ public class DrawPanel extends DynamicPanel {
         Dimension panelSize = this.getSize();
         g.setColor(Color.red);
         g.drawLine(0, 0, panelSize.width, panelSize.height);
+        drawClickPoints(g);
+        drawMouseInfo(g);
+    }
+
+    private void drawMouseInfo(@NotNull Graphics g) {
         g.setColor(getPanelColors().getSecondaryColor());
         g.drawString("x: " + mousePos[0] + ", y: " + mousePos[1], mousePos[0], mousePos[1]);
         g.drawString("zoom: " + zoom, mousePos[0], mousePos[1] + 15);
+    }
+
+    private void drawClickPoints(@NotNull Graphics g) {
+        int radius = 10;
+        g.setColor(Color.red);
+        for (int[] point : StaticData.clickPoints) {
+            g.fillOval(
+                    point[0] - radius / 2,
+                    point[1] - radius / 2,
+                    radius,
+                    radius);
+        }
     }
 }
---
+++ src/main/java/graphics/staticData/StaticData.java
@@ -0,0 +1,8 @@
+package graphics.staticData;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class StaticData {
+    public static List<int[]> clickPoints = new ArrayList<>();
+}
