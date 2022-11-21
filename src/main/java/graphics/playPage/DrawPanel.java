379066b526346a1525b33c12a889e763ba8f536b
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-11-21 22:28:21 +0200

    zoom listener

--- src/main/java/graphics/playPage/DrawPanel.java
+++ src/main/java/graphics/playPage/DrawPanel.java
@@ -4,18 +4,28 @@ import graphicsEngine.colors.SimpleColorScheme;
 import graphicsEngine.panels.DynamicPanel;
 
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.*;
+
+import org.jetbrains.annotations.NotNull;
 
 //TODO: add javadocs
 public class DrawPanel extends DynamicPanel {
-    private static Color backgroundColor = new Color(50, 100, 150);
-    private int[] mousePos = new int[2];
+    private static final Color BACKGROUND_COLOR = new Color(50, 100, 150);
+    private static final int[] ZOOM_LIMITS = new int[] {1, 10};
+    private static final int INITIAL_ZOOM = 5, ZOOM_INCREMENT = 1;
+    private final int[] mousePos = new int[2];
+    private int zoom = INITIAL_ZOOM;
 
     //TODO: add javadoc
     DrawPanel() {
-        super(new SimpleColorScheme(backgroundColor, null));
-        addMouseMotionListener(new MouseMotionListener() {
+        super(new SimpleColorScheme(BACKGROUND_COLOR, null));
+        addMouseMotionListener(getNewMouseMotionListener());
+        addMouseWheelListener(getNewMouseWheelListener());
+        //setBackground(backgroundColor);
+    }
+
+    private @NotNull MouseMotionListener getNewMouseMotionListener() {
+        return new MouseMotionListener() {
             @Override
             public void mouseDragged(MouseEvent e) {
                 mousePos[0] = e.getX();
@@ -27,8 +37,24 @@ public class DrawPanel extends DynamicPanel {
                 mousePos[0] = e.getX();
                 mousePos[1] = e.getY();
             }
-        });
-        //setBackground(backgroundColor);
+        };
+    }
+
+    private @NotNull MouseWheelListener getNewMouseWheelListener() {
+        return new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                zoom = getNewZoom(zoom, e.getWheelRotation() * ZOOM_INCREMENT);
+            }
+
+            private int getNewZoom(int previous, int delta) {
+                return Math.max(
+                        ZOOM_LIMITS[0],
+                        Math.min(
+                                ZOOM_LIMITS[1],
+                                previous + delta));
+            }
+        };
     }
 
     //TODO: add javadoc
@@ -40,5 +66,6 @@ public class DrawPanel extends DynamicPanel {
         g.drawLine(0, 0, panelSize.width, panelSize.height);
         g.setColor(getPanelColors().getSecondaryColor());
         g.drawString("x: " + mousePos[0] + ", y: " + mousePos[1], mousePos[0], mousePos[1]);
+        g.drawString("zoom: " + zoom, mousePos[0], mousePos[1] + 15);
     }
 }
