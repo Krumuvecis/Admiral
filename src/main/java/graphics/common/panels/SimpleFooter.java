6d07922bb7506f251cced977fe45f51968706e14
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2023-01-27 19:08:20 +0200

    footer improvements

--- src/main/java/graphics/common/panels/SimpleFooter.java
+++ src/main/java/graphics/common/panels/SimpleFooter.java
@@ -1,5 +1,26 @@
 package graphics.common.panels;
 
-public class SimpleFooter {
-    //TODO: finish this
+import org.jetbrains.annotations.Nullable;
+
+import graphicsEngine.colors.SimpleColorScheme;
+import graphicsEngine.panels.BorderProperties;
+import graphicsEngine.pages.panels.AbstractFooter;
+
+//TODO: finish this
+public class SimpleFooter extends AbstractFooter {
+    private static final int HEIGHT = 40;
+
+    /**
+     * TODO: finish this javadoc
+     */
+    public SimpleFooter(@Nullable SimpleColorScheme colors,
+                        @Nullable BorderProperties borderProperties) {
+        super(colors, HEIGHT, borderProperties);
+    }
+
+    /**
+     * TODO: finish this javadoc
+     */
+    @Override
+    public final void addParts() {}
 }
